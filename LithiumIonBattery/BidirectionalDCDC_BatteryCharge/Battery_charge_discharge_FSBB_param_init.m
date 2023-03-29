efb49f74b7d8c13b4eb4f7a8c26507d12913d11c
LokiXun <2682414501@qq.com>
2023-03-29 22:31:53 +0800

    fix:1. set capacitor's initial voltage to solve the battery side voltage start from 0

--- LithiumIonBattery/BidirectionalDCDC_BatteryCharge/Battery_charge_discharge_FSBB_param_init.m
+++ LithiumIonBattery/BidirectionalDCDC_BatteryCharge/Battery_charge_discharge_FSBB_param_init.m
@@ -20,7 +20,7 @@ clear;clc;close all;
 %     floor(BusVoltageReference * BusVoltageErrorRange); % <5%
 
 %% 2. control module param
-wave_freq = 45e3;  % 方波频率
+wave_freq = 2 * 45e3;  % 方波频率
 time_step = 1 / wave_freq;  %时间常数>>求导
 
 fprintf("FSBB initialize success!")
@@ -32,5 +32,50 @@ fprintf("FSBB initialize success!")
 % u1 == 0 & (u3>=85.5 & u3 <=101)
 % u1 == 0 & (u3 > 101 & u3 <=116.15)
 
+%% Load change in Discharge
+% 1. Constant Current
+Voltage_current_mode = 96;
+Current_100 = 29;
+R_100 = Voltage_current_mode / Current_100;
+load_percent_list = [1/4, 1/2, 3/4, 1];
+R_current_mode_load_change = zeros(1, length(load_percent_list));
+I_current_mode_load_change = zeros(1, length(load_percent_list));
+R_current_mode_load_change(end) = R_100;
+I_current_mode_load_change(end) = Current_100;
+for i=1:length(load_percent_list)-1
+    Load_percent = load_percent_list(i);
+    R_current_mode_load_change(i) = R_100 / Load_percent;
+    I_current_mode_load_change(i) = Current_100 * Load_percent;
+end
 
+
+% 2. Constant Power
+Voltage_power_mode = 105;
+P_100 = 3000;
+R_100 = Voltage_power_mode^2 / P_100;
+load_percent_list = [1/4, 1/2, 3/4, 1];
+R_power_mode_load_change = zeros(1, length(load_percent_list));
+Power_power_mode_load_change = zeros(1, length(load_percent_list));
+R_power_mode_load_change(end) = R_100;
+Power_power_mode_load_change(end) = P_100;
+for i=1:length(load_percent_list)-1
+    Load_percent = load_percent_list(i);
+    R_power_mode_load_change(i) = R_100 / Load_percent;
+    Power_power_mode_load_change(i) = P_100 * Load_percent;
+end
+
+% 3. Constant Voltage
+Voltage_setup = 96;
+P_100 = 3000;
+R_100 = Voltage_setup^2 / P_100;
+load_percent_list = [1/4, 1/2, 3/4, 1];
+R_voltage_mode_load_change = zeros(1, length(load_percent_list));
+Power_voltage_mode_load_change = zeros(1, length(load_percent_list));
+R_voltage_mode_load_change(end) = R_100;
+Power_voltage_mode_load_change(end) = P_100;
+for i=1:length(load_percent_list)-1
+    Load_percent = load_percent_list(i);
+    R_voltage_mode_load_change(i) = R_100 / Load_percent;
+    Power_voltage_mode_load_change(i) = P_100 * Load_percent;
+end
 
